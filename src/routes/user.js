87f0344440a439806407e4b9e41ddfbf093f5e11
prateek10-04 <118262131+prateek10-04@users.noreply.github.com>
2024-01-16 23:07:49 +0530

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -89,8 +89,8 @@ const passport=require('passport')
 const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 
 passport.use(new GoogleStrategy({
-    clientID:process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    clientID:'222139524192-e53bblvjqvon3jvnak2m70l0kvjt6f5g.apps.googleusercontent.com',
+    clientSecret:'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',
     callbackURL: "http://localhost:3000/auth/google/callback",
     passReqToCallback : true
   },
