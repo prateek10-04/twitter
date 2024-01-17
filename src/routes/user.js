95cdd2999380801ad24158cc94cb77a2c529eff8
prateek10-04 <118262131+prateek10-04@users.noreply.github.com>
2024-01-17 21:34:39 +0530

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -92,7 +92,7 @@ const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: "https://twitter-clone-1kj1.onrender.com/auth/google/callback",
     passReqToCallback : true
   },
   function(request, accessToken, refreshToken, profile, done) {
